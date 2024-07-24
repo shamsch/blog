cdd8474380421a9656ad376c7bb7fc0008dec425
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2024-07-25 00:55:59 +0300

    Update README.md to reflect blog changes

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# Blog
+# Blog (and more!)
 
-Template from and deployed with [Vercel](https://vercel.com/)
+Template from and deployed with [Vercel](https://vercel.com/) and domain managed by [AWS](https://aws.amazon.com/) Route 53.
+
+![AWS](https://partner.zoom.us/wp-content/uploads/2022/12/2022_Zoom-AWS_Lockup_RGB-1-e1672857797889-1024x760.png)
+![Vercel](https://logowik.com/content/uploads/images/vercel1868.jpg)
--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Page() {
 			</h1>
 			<p className='mb-4'>
 				{`I am software developer based in Finland. Currently working as a software consultant. This 
-        is my web presence. There is a blog section where I write about code and life. If you are interested,
-        reach out to me via one of platforms below. Happy to have a chat. Thanks for visiting.`}
+        is my minimal web presence. There is a blog section where I write about code and life. If you are interested,
+        reach out to me via one of the platforms below. Happy to have a chat. Thanks for visiting!`}
 			</p>
 			<div className='my-8'>
 				<BlogPosts />
