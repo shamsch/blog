79c341636489f81fe81dd9a8c8803fa5529fbf21
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2024-07-24 17:38:56 +0300

    init

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import { BlogPosts } from "app/components/posts";
+
+export default function Page() {
+	return (
+		<section>
+			<h1 className='mb-8 text-2xl font-semibold tracking-tighter'>
+				Who is Shamsur?
+			</h1>
+			<p className='mb-4'>
+				{`I am software developer based in Finland. Currently working as a software consultant. This 
+        is my web presence. There is a blog section where I write about code and life. If you are interested,
+        reach out to me via one of platforms below. Happy to have a chat. Thanks for visiting.`}
+			</p>
+			<div className='my-8'>
+				<BlogPosts />
+			</div>
+		</section>
+	);
+}
